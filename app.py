3f838883efd5c0087cc1124ec405df6518293bb4
zhouc <zhoucheng1122@outlook.com>
2024-08-01 14:57:59 +0800

    addlist

--- app.py
+++ app.py
@@ -1,12 +1,54 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://summer_admin'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template('PDFAnalysis.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email-l']
+        password = request.form['password-l']
+        user = User.query.filter_by(email=email, password=password).first()
+        if user:
+            return redirect(url_for('home'))
+        else:
+            return 'Invalid credentials'
+    return render_template('login.html')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email-s']
+        password = request.form['password-s']
+        new_user = User(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    # 初始化数据库
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
